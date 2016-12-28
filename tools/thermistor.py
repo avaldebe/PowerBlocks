49384b917aba979f02b028ad567967f9a89b7274
Alvaro Valdebenito <alvarov@met.no>
2016-12-28 15:10:16 +0100

    use class decorators
    decode SMD resistor markings

--- tools/thermistor.py
+++ tools/thermistor.py
@@ -2,75 +2,120 @@
 """
 NTC thermistor class
 
-Based on https://pypi.python.org/pypi/thermistor
+TODO:
+- voltage and current SI_param objects
+- voltage calculation for R_bias, R_load, R_bridge NTC configurations
 """
 from __future__ import print_function
 import numpy
 
 class SI_param(object):
-    """Generic SI object, such as temperature and resistence"""
+    """
+    Generic SI object, such as temperature and resistence
+    """
+    _unit_default = None
     _unit_conv = {}
     def __init__(self, value, unit=None):
         assert unit in self._unit_conv, 'Unsuported unit "%s"'%unit
-        self._value = value
-        self._unit = unit
-
-    def unit(self, unit=None):
-        assert unit in self._unit_conv, 'Unsuported unit "%s"'%unit
-        value = self._value
-        if unit != self._unit:
-            value *= self._unit_conv[self._unit]['scale']
-            value += self._unit_conv[self._unit]['offset']
-            value -= self._unit_conv[unit]['offset']
-            value /= self._unit_conv[unit]['scale']
-        return value
-
+        self._value = float(value)
+        self._unit = unit if unit else self._unit_default
     def __str__(self):
-        return '%.3f %s'%(self._value,self._unit)
+        return '%g %s'%(self._value, self._unit)
+    def __float__(self):
+        return self._value
+
+    @classmethod
+    def convert_unit(cls, value, unit_from, unit_to):
+        def _from_unit(val, unit):
+            assert unit in cls._unit_conv, 'Unsuported unit "%s"'%unit
+            scale, offset = cls._unit_conv[unit]['scale'], cls._unit_conv[unit]['offset']
+            return scale * val + offset
+        def _to_unit(val, unit):
+            assert unit in cls._unit_conv, 'Unsuported unit "%s"'%unit
+            scale, offset = cls._unit_conv[unit]['scale'], cls._unit_conv[unit]['offset']
+            return (val - offset) / scale
+        if unit_from is unit_to:
+            return value
+        else:
+            return _to_unit(_from_unit(value, unit_from), unit_to)
+
+    def unit(self, unit):
+        value = self.convert_unit(self._value, self._unit, unit)
+        self.__init__(value, unit)
+        return self
 
 class Resistance(SI_param):
     """Resistance in ohm"""
+    _unit_default = 'kohm'
     _unit_conv = dict(
         Mohm={'scale':1e6, 'offset':0},
         kohm={'scale':1e3, 'offset':0},
         ohm={'scale':1e0, 'offset':0},
         mohm={'scale':1e-3, 'offset':0},
     )
-    def __init__(self, value, unit='kohm'):
-        super(Resistance, self).__init__(value, unit)
+    @staticmethod
+    def smd(code):
+        """
+        Decode 3-digit and 4-digit SMD markings
+        http://www.hobby-hour.com/electronics/smdcalc.php#smd_resistor_code
+        """
+        assert type(code) is str and len(code) in [3, 4], 'Unknown SMD code "%s"'%code
+        if 'R' in code:
+            value = code.replace('R','.')
+        elif code.isdigit():
+            value = "%se%s"%(code[0:-1], code[-1])
+        try:
+            return Resistance(value, 'ohm')
+        except ValueError as error:
+            print('Wrong SMD code "%s"'%code)
+            raise error
+    @classmethod
+    def convert_unit(cls, value, unit_from, unit_to):
+        if unit_from.lower() in ['smd', 'smd3', 'smd4']:
+            return float(cls.smd(value).unit(unit_to))
+        else:
+            return super(Resistance, cls).convert_unit(value, unit_from, unit_to)
 
 class Temperature(SI_param):
     """Temperature in *K"""
+    _unit_default = 'K'
     _unit_conv = dict(
         K={'scale':1, 'offset':0},
         C={'scale':1, 'offset':273.15},
         F={'scale':5/9,'offset':459.67*5/9},
     )
-    def __init__(self, value, unit='K'):
-        super(Temperature, self).__init__(value, unit)
 
 class NTC(object):
-    """Generig NTC"""
-    def __init__(self, R0, B, T0=298.15, T1=323.15):
+    """
+    Generig NTC
+    Based on https://pypi.python.org/pypi/thermistor
+    """
+    def __init__(self, B, R0, unit='kohm', T0=298.15, T1=323.15, name=None):
         """Basic NTC parameters"""
-        self.R0 = float(R0)   # resistence at T0 in [kOhm]
-        self.B = float(B)     # beta(T0/T1) coefficient in [K]
-        self.T0 = float(T0)   # 25 [*C] in [*K]
-        self.T1 = float(T1)   # 50 [*C] in [*K]
-#       self.R1 = self.resistance(self.T1,'K')
+        self.B = float(B)       # beta(T0/T1) coefficient in [K]
+        self.R0 = Resistance.convert_unit(R0, unit, 'kohm') # resistence at T0 in [kOhm]
+        self.T0 = float(T0)     # 25 [*C] in [*K]
+        self.T1 = float(T1)     # 50 [*C] in [*K]
+        self.name = name if name else 'NTC%d'%self.B
+#       self.R1 = self.resistance(self.T1, 'K')
 
     def temperature(self, R, r_unit='kohm', t_unit='C'):
         """resistance R [kohm] to temperature [unit]"""
-        r_kohm = Resistance(R, r_unit).unit('kohm')
+        r_kohm = Resistance.convert_unit(R, r_unit, 'kohm')
         temp_inv = numpy.log(r_kohm/self.R0)/self.B + 1/self.T0
-        return Temperature(1/temp_inv, 'K').unit(t_unit)
+        return Temperature.convert_unit(1/temp_inv, 'K', t_unit)
 
     def reststance(self, T, r_unit='kohm', t_unit='C'):
         """temperature [unit] to resistance R [kOhm] """
-        temp = Temperature(T, t_unit).unit('K')
+        temp = Temperature.convert_unit(T, t_unit, 'K')
         temp = 1/temp - 1/self.T0
         r_kohm = numpy.exp(self.B*temp)
-        return Resistance(r_kohm, 'kohm').unit(r_unit)
+        return Resistance.convert_unit(r_kohm, 'kohm', r_unit)
+
+    def __str__(self):
+        t0 = Temperature(self.T0, 'K').unit('C')
+        r0 = Resistance(self.R0, 'kohm')
+        return '%s(%s)=%s'%(self.name, t0, r0)
 
 
 class TTC05(NTC):
@@ -78,46 +123,38 @@ class TTC05(NTC):
     TKS TTC05 series
     http://www.thinking.com.tw/documents/en-TTC05.pdf
     """
-    __catalog = {
-        '005':NTC(.005, 2400), '010':NTC(.010, 2800), '015':NTC(.015, 2800),
-        '020':NTC(.020, 2800), '025':NTC(.025, 2900), '045':NTC(.045, 3100),
-        '050':NTC(.050, 3100), '060':NTC(.060, 3100), '085':NTC(.085, 3200),
-        '090':NTC(.090, 3200), '101':NTC(.100, 3200), '121':NTC(.120, 3300),
-        '151':NTC(.150, 3300), '201':NTC(.200, 3500), '221':NTC(.220, 3500),
-        '251':NTC(.250, 3500), '301':NTC(.300, 3800), '471':NTC(.470, 3500),
-        '501':NTC(.500, 3700), '681':NTC(.680, 3800), '701':NTC(.700, 3800),
-        '102':NTC(1.00, 3800), '152':NTC(1.50, 3950), '202':NTC(2.00, 4000),
-        '222':NTC(2.20, 4000), '252':NTC(2.50, 4000), '302':NTC(3.00, 4000),
-        '332':NTC(3.30, 4000), '402':NTC(4.00, 4000), '472':NTC(4.70, 4050),
-        '502':NTC(5.00, 3950), '602':NTC(6.00, 4050), '682':NTC(6.80, 4050),
-        '802':NTC(8.00, 4050), '103':NTC(10.0, 4050), '123':NTC(12.0, 4050),
-        '153':NTC(15.0, 4150), '203':NTC(20.0, 4250), '303':NTC(30.0, 4250),
-        '473':NTC(47.0, 4300), '503':NTC(50.0, 4300), '104':NTC(100., 4400),
-        '154':NTC(150., 4500), '204':NTC(200., 4600), '224':NTC(220., 4600),
-        '474':NTC(470., 4750),
+    _catalog = { # SMD_code:beta_coff
+        '005':2400, '010':2800, '015':2800, '020':2800, '025':2900, '045':3100,
+        '050':3100, '060':3100, '085':3200, '090':3200, '101':3200, '121':3300,
+        '151':3300, '201':3500, '221':3500, '251':3500, '301':3800, '471':3500,
+        '501':3700, '681':3800, '701':3800, '102':3800, '152':3950, '202':4000,
+        '222':4000, '252':4000, '302':4000, '332':4000, '402':4000, '472':4050,
+        '502':3950, '602':4050, '682':4050, '802':4050, '103':4050, '123':4050,
+        '153':4150, '203':4250, '303':4250, '473':4300, '503':4300, '104':4400,
+        '154':4500, '204':4600, '224':4600, '474':4750,
     }
-
-    def catalog(self, model=None):
+    @classmethod
+    def catalog(cls, model=None):
         """All NTCs in series"""
         if model is None:
-            return self.__catalog.keys()
+            return cls._catalog.keys()
+        elif model in cls._catalog:
+            return cls._catalog[model]
         else:
-            return self.__catalog[model]
+            print('Unknown NTC model %s'%model)
+            raise KeyError
 
-    def __init__(self, model):
+    def __init__(self, code):
         self.vendor = 'TKS'
         self.series = 'TTC05'
-        self.model = "%03d"%int(model)
+        self.model = "%03d"%int(code)
         self.name = '%s%s'%(self.series, self.model)
         try:
-            ntc = self.catalog(self.model)
+            beta = self.catalog(self.model)
         except KeyError as error:
             print('Unknown NTC %s%s'%self.name)
             raise error
-        super(TTC05, self).__init__(ntc.R0, ntc.B)
-
-    def __str__(self):
-        return '%s: %s'%(self.name, super(TTC05, self).__str__())
+        super(TTC05, self).__init__(beta, self.model, unit='SMD3', name=self.name)
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plot
@@ -127,10 +164,11 @@ if __name__ == '__main__':
         index=numpy.linspace(25, 75, 100),  # 25 to 75 *C
     )
 
-    for model in [102, 103, 104]:
-        ntc=TTC05(model)
+    for ntc in [102, 103, 104]:
+        ntc=TTC05(ntc)
+        print("%s"%ntc)
         catalog[ntc.name] = pandas.Series(
-            ntc.reststance(catalog.index)*1e3,  # kOhm to Ohm
+            ntc.reststance(catalog.index, 'ohm'),
             index=catalog.index,
         )
 
